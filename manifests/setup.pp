8bfdd9d7ab74bab91cd1339039caf9acc2551c72
akshaybhandari <akshayb.2693@gmail.com>
2015-04-28 19:59:32 +0530

    Modified the module for compute only checks. These changes were necessary as new base module is introduced.

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,15 +1,4 @@
 class nagioscompute::setup {
-  file { '/etc/nagios/nrpe.d/nrpe_command.cfg':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    notify  => Service['nagios-nrpe-server'],
-    require => Package['nagios-nrpe-server'],
-  }
-  class { 'nagioscompute::setup::host':
-    require => [ File['/etc/nagios/nrpe.cfg'], Package['nagios-nrpe-plugin', 'nagios-nrpe-server'] ],
-  } ->
   class { 'nagioscompute::setup::services':
     service_list          => $::nagioscompute::service_list,
   } ->
--- manifests/setup/files.pp
+++ manifests/setup/files.pp
@@ -1,16 +1,8 @@
 class nagioscompute::setup::files {
-  file { "nagios_privileged_${hostname}":
-    ensure => directory,
-    path   => '/usr/lib/nagios/plugins/privileged/',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    require => Package['nagios-nrpe-server'],
-  }
-  file { "check_memory.sh_${hostname}":
+  file { "check_service.sh_${hostname}_compute":
     ensure => present,
-    path   => '/usr/lib/nagios/plugins/check_memory.sh',
-    source => 'puppet:///modules/nagioscompute/check_memory.sh',
+    path   => '/usr/lib/nagios/plugins/check_service.sh',
+    source => 'puppet:///modules/nagioscompute/check_service.sh',
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
@@ -31,13 +23,5 @@ class nagioscompute::setup::files {
     group   => 'root',
     mode    => '0755',
     require => File["nagios_privileged_${hostname}"]
-  }
-  file { "check_service.sh_${hostname}":
-    ensure => present,
-    path   => '/usr/lib/nagios/plugins/check_service.sh',
-    source => 'puppet:///modules/nagioscompute/check_service.sh',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
   }
 }
